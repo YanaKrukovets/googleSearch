9d7c2d3d823752e17a639ff29e3bdb85dc39d4cd
Yana Krukovets <yanashelli@gmail.com>
2022-12-09 11:51:50 -0500

    Change API

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -11,10 +11,10 @@ export const Results = () => {
 
     useEffect(() => {
         if (searchTerm !== '') {
-            if (location.pathname === '/search') {
-              getResults(`/organicResults?query=${searchTerm}&num=10`);
+            if (location.pathname == '/images') {
+              getResults(`imagesearch?query=${searchTerm}&num=10`);
             } else {
-                getResults(`${location.pathname}?query=${searchTerm}&num=10`)
+              getResults(`${location.pathname}?query=${searchTerm}&num=10`);
             }
           }
         }, [searchTerm, location.pathname]);
@@ -25,7 +25,7 @@ export const Results = () => {
         case '/search':
             return (
                 <div className="sm:px-56 flex flex-wrap justify-between space-y-6">
-                    {results?.organic_results?.map(({ title, link }, index) => (
+                    {results?.items?.map(({ title, link }, index) => (
                         <div key={index} className="md:w-2/5 w-full">
                             <a href={link} target="_blank" rel="noreferrer">
                                 <p className="text-sm">{link.length > 30 ? link.substring(0, 30) : link}</p>
@@ -38,7 +38,7 @@ export const Results = () => {
         case '/images':
             return (
                 <div className="flex flex-wrap justify-center items-center">
-                  {results?.image_results?.map(({ image, title, link }, index) => (
+                  {results?.items?.map(({ image, title, link }, index) => (
                     <a href={link} target="_blank" key={index} rel="noreferrer" className="sm:p-3 p-5">
                       <img src={image} alt={title} loading="lazy" />
                       <p className="sm:w-36 w-36 break-words text-sm mt-2">{title}</p>
@@ -47,7 +47,7 @@ export const Results = () => {
                 </div>
               );
 
-        case '/videos':
+    /*    case '/videos':
          
             return (
                 <div className="flex flex-wrap ">
@@ -57,7 +57,7 @@ export const Results = () => {
                     </div>
                   ))}
                 </div>
-              );
+              );*/
     
         default:
             return 'ERROR';
